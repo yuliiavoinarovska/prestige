a1bf3092aae00355fc7def41728f1247805d390e
Julia <uvojnarovskaa@gmail.com>
2024-12-22 13:26:44 +0200

    upd form

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,49 @@
 // BURGER MENU
-const menu = document.querySelector('.nav');
-const menuItem = document.querySelectorAll('.nav__item');
-const hamburger = document.querySelector('.nav-btn');
+const menu = document.querySelector(".nav");
+const menuItem = document.querySelectorAll(".nav__item");
+const hamburger = document.querySelector(".nav-btn");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('nav-btn--active');
-  menu.classList.toggle('nav--active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("nav-btn--active");
+  menu.classList.toggle("nav--active");
 });
 
 menuItem.forEach((item) => {
-  item.addEventListener('click', () => {
-    hamburger.classList.toggle('nav-btn--active');
-    menu.classList.toggle('nav--active');
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("nav-btn--active");
+    menu.classList.toggle("nav--active");
   });
 });
 
 //MODAL
-const modalTrigger = document.querySelectorAll('[data-modal]');
-const modal = document.querySelector('.modal');
-const modalCloseBtn = document.querySelector('[data-close]');
+const modalTrigger = document.querySelectorAll("[data-modal]");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector("[data-close]");
 
-modalTrigger.forEach(btn => {
-  btn.addEventListener('click', () => {
-    modal.classList.add('modal--show');
-    modal.classList.remove('modal--hide');
-    document.body.style.overflow = 'hidden';
+modalTrigger.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.classList.add("modal--show");
+    modal.classList.remove("modal--hide");
+    document.body.style.overflow = "hidden";
   });
 });
 
 function closeModal() {
-  modal.classList.add('modal--hide');
-  modal.classList.remove('modal--show');
-  document.body.style.overflow = '';
+  modal.classList.add("modal--hide");
+  modal.classList.remove("modal--show");
+  document.body.style.overflow = "";
 }
 
-modalCloseBtn.addEventListener('click', closeModal);
+modalCloseBtn.addEventListener("click", closeModal);
 
-modal.addEventListener('click', (e) => {
+modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     closeModal();
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'Escape') {
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
     closeModal();
   }
 });
